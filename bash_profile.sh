e7d3cc472e5bc882456676f97d4107a14be10289
Matheus Portela <matheus.v.portela@gmail.com>
2016-10-31 12:23:54 -0200

    Include bash and Sublime settings

---
+++ bash_profile.sh
@@ -0,0 +1,33 @@
+# Productivity
+
+## Terminal
+alias ll='ls -lh'
+
+## Git
+alias gs='git status'
+alias ga='git add --all'
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit --all'
+alias gps='git push'
+alias gpl='git pull'
+alias gco='git checkout'
+alias gf='git fetch'
+alias gsub='git submodule sync && git submodule update --init --recursive --remote'
+alias gb='git branch'
+alias gcb='git checkout -b'
+alias gbd='git branch -d'
+alias gl='git log'
+alias gg='git grep'
+
+## Git Flow
+alias gffs='git flow feature start'
+alias gfff='git flow feature finish'
+alias gffps='git flow feature publish'
+alias gffpl='git flow feature pull'
+alias gfrs='git flow release start'
+alias gfrps='git flow release publish'
+alias gfrt='git flow release track'
+alias gfrf='git flow release finish'
+alias gfhfs='git flow hotfix start'
+alias gfhff='git flow hotfix finish'
